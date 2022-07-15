bb782971cb4d9bce36c2e55cb7565b9c052f4d45
Cheppar <ejioforched@gmail.com>
2022-07-15 23:09:17 +0300

    UI

--- js/data.js
+++ js/data.js
@@ -1,12 +1,14 @@
 
 
-fetch('https://livecapture-420.s3.amazonaws.com/2022-07-02_105342_34.6771060209393_-111.97143420594738.json2').then((data)=>{
+fetch('https://livecapture-420.s3.amazonaws.com/2022-07-02_104904_34.671924815774275_-111.94961049960204.json2').then((data)=>{
 	//console.log(data);
 	return data.json();
 }).then((completedata)=>{
-		 //console.log(completedata.METADATA.TOUR_NAME);
-		 //document.getElementById('tit').innerHTML=completedata.METADATA.TOUR_NAME;
+		 document.getElementById('pat').value=completedata.METADATA.TOUR_NAME;
+		 document.getElementById('tourguide').value=completedata.METADATA.GUIDE_NAME;
+		 document.getElementById('photographer').value=completedata.METADATA.LOCATION.PLACE;
 		 
+
 	}).catch((err)=>{
 		console.log(err);
 	});
